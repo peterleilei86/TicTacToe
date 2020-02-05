aa9619aced990507e47d36e48796b924fdfc7e33
Peter Wang <petewan3@cisco.com>
2020-02-05 10:27:07 -0500

    remove renderSquare duplication

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,33 +1,27 @@
-import React from 'react'
-import Square from './Square'
-import {Tiles} from './Game'
+import React from 'react';
+import Square from './Square';
+import { Tiles } from './Game';
 
 type BoardProps = {
-  squares: Tiles
-  onClick: (index: number) => void
-}
+	squares: Tiles;
+	onClick: (index: number) => void;
+};
 
-const Board = ({squares, onClick}: BoardProps) => {
-  const renderSquare = (index: number) => {
-    const handleClick = (e: React.SyntheticEvent) => onClick(index)
-    return <Square value={squares[index]} onClick={handleClick} />
-  }
+const Board = ({ squares, onClick }: BoardProps) => {
+	const renderSquare = (index: number) => {
+		const handleClick = (e: React.SyntheticEvent) => onClick(index);
+		return <Square value={squares[index]} onClick={handleClick} />;
+	};
 
-  return (
-    <div className='flex items-center justify-center'>
-      <div className='w-2/5 h-auto flex flex-wrap'>
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className='flex items-center justify-center'>
+			<div className='w-2/5 h-auto flex flex-wrap'>
+				{Array(9)
+					.fill(null)
+					.map((_, i) => renderSquare(i))}
+			</div>
+		</div>
+	);
+};
 
-export default Board
+export default Board;
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,83 +1,83 @@
-import React, {useState, useEffect} from 'react'
-import Board from './Board'
-import {calcWinner, calMove} from '../utils'
+import React, { useState, useEffect } from 'react';
+import Board from './Board';
+import { calcWinner, calMove } from '../utils';
 
-export type CurrentPlayer = 'X' | 'O'
-export type Tile = CurrentPlayer | null
-export type Tiles = Tile[]
-type Winner = CurrentPlayer | 'draw' | null
+export type CurrentPlayer = 'X' | 'O';
+export type Tile = CurrentPlayer | null;
+export type Tiles = Tile[];
+type Winner = CurrentPlayer | 'draw' | null;
 
-const COMPUTER = 'O'
-const HUMAN = 'X'
+const COMPUTER = 'O';
+const HUMAN = 'X';
 
 const Game: React.FC = () => {
-  const [squares, setSquares] = useState<Tiles>(Array(9).fill(null))
-  const [currentPlayer, setCurrentPlayer] = useState<CurrentPlayer>(HUMAN)
-  const [winner, setWinner] = useState<Winner>(null)
+	const [squares, setSquares] = useState<Tiles>(Array(9).fill(null));
+	const [currentPlayer, setCurrentPlayer] = useState<CurrentPlayer>(HUMAN);
+	const [winner, setWinner] = useState<Winner>(null);
 
-  const handleClick = (index: number) => {
-    if (winner) {
-      return
-    }
-    const newSquares = squares.map((s: Tile, i: number) => (i === index ? currentPlayer : s))
-    setSquares(newSquares)
-    const newCurrentPlayer = currentPlayer === 'X' ? 'O' : 'X'
-    setCurrentPlayer(newCurrentPlayer)
-  }
+	const handleClick = (index: number) => {
+		if (winner) {
+			return;
+		}
+		const newSquares = squares.map((s: Tile, i: number) => (i === index ? currentPlayer : s));
+		setSquares(newSquares);
+		const newCurrentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+		setCurrentPlayer(newCurrentPlayer);
+	};
 
-  const makeMove = (squares: Tiles) => {
-    const index = calMove(squares)
-    if (index >= 0) {
-      const newSquares = squares.map((s, i) => {
-        if (i === index) {
-          return COMPUTER
-        }
-        return s
-      })
-      setSquares(newSquares)
-      setCurrentPlayer(HUMAN)
-    }
-  }
+	const makeMove = (squares: Tiles) => {
+		const index = calMove(squares);
+		if (index >= 0) {
+			const newSquares = squares.map((s, i) => {
+				if (i === index) {
+					return COMPUTER;
+				}
+				return s;
+			});
+			setSquares(newSquares);
+			setCurrentPlayer(HUMAN);
+		}
+	};
 
-  const handleReset = (e: React.SyntheticEvent) => {
-    setSquares(Array(9).fill(null))
-    setCurrentPlayer(HUMAN)
-    setWinner(null)
-  }
+	const handleReset = (e: React.SyntheticEvent) => {
+		setSquares(Array(9).fill(null));
+		setCurrentPlayer(HUMAN);
+		setWinner(null);
+	};
 
-  useEffect(() => {
-    if (calcWinner(squares)) {
-      setWinner(calcWinner(squares))
-    } else if (!calcWinner(squares) && squares.every(s => s !== null)) {
-      setWinner('draw')
-    } else if (currentPlayer === COMPUTER) {
-      makeMove(squares)
-    }
-  }, [squares, currentPlayer])
+	useEffect(() => {
+		if (calcWinner(squares)) {
+			setWinner(calcWinner(squares));
+		} else if (!calcWinner(squares) && squares.every(s => s !== null)) {
+			setWinner('draw');
+		} else if (currentPlayer === COMPUTER) {
+			makeMove(squares);
+		}
+	}, [squares, currentPlayer]);
 
-  const message =
-    winner === 'X'
-      ? 'You win!'
-      : winner === 'O'
-      ? 'Computer win!'
-      : winner === 'draw'
-      ? "It's a draw!"
-      : ''
-  return (
-    <div className='container w-1/2 py-3 border-2 my-3'>
-      <div className='h-20 relative'>
-        <h2 className='text-center font-bold sm:text-xl md:text-2xl  lg:text-3xl'>Tic Tac Toe</h2>
-        <button
-          className='bg-green-400 hover:bg-green-600 text-white font-bold px-2 py-2 mx-10 rounded absolute right-0 top-0'
-          onClick={handleReset}
-        >
-          Reset
-        </button>
-      </div>
-      <Board squares={squares} onClick={(index: number) => handleClick(index)} />
-      {winner && <p className='text-center font-bold text-3xl text-green-400 my-5'>{message}</p>}
-    </div>
-  )
-}
+	const message =
+		winner === 'X'
+			? 'You win!'
+			: winner === 'O'
+			? 'Computer win!'
+			: winner === 'draw'
+			? "It's a draw!"
+			: '';
+	return (
+		<div className='container w-1/2 py-3 border-2 my-3'>
+			<div className='h-20 relative'>
+				<h2 className='text-center font-bold sm:text-xl md:text-2xl lg:text-3xl'>Tic Tac Toe</h2>
+				<button
+					className='w-24 h-12 bg-green-400 hover:bg-green-600 text-white font-bold text-xl px-2 py-2 mx-10 rounded absolute right-0 top-0'
+					onClick={handleReset}
+				>
+					Reset
+				</button>
+			</div>
+			<Board squares={squares} onClick={(index: number) => handleClick(index)} />
+			{winner && <p className='text-center font-bold text-3xl text-green-400 my-5'>{message}</p>}
+		</div>
+	);
+};
 
-export default Game
+export default Game;
